71348ef9cc22a98c8e6753ad85388085c79ca826
philipfarmer <philip@unthink.ie>
2023-01-06 14:50:59 +0000

    aa
    aaa

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <div>Next JS &amp; WordPress course.</div>;
+export default function Home(props) {
+  console.log("PROPS: ", props);
+  return <div>Next JS &amp; WordPress course. Test</div>;
 }
+
+export const getStaticProps = async () => {
+ const {data} = await client.query({
+   query: gql`
+   query NewQuery {
+     pages {
+       nodes {
+         title
+       }
+     }
+   }
+   `
+ })
+  return {
+    props: {
+      data,
+      myexampleprop: "test",
+    },
+  };
+};
